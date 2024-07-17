1b2c2cc80eaedf0331e8053a19986dc258638647
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-16 22:48:39 -0500

    Añadiendo atributos a clase Cancion

---
+++ lista_reproduccion/src/Cancion.java
@@ -0,0 +1,21 @@
+public class Cancion {
+    String artist_name;
+    String track_name;
+    int popularity;
+    int year;
+    String genre;
+    double danceability;
+    double energy;
+    int key;
+    double loudness;
+    int mode;
+    double speechiness;
+    double acousticness;
+    String instrumentalness;
+    double liveness;
+    double valence;
+    double tempo;
+    int duration_ms;
+    int time_signature;
+    
+}
