27f7d3173b19f593c43f649e14369583d2b4910e
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-17 22:51:49 -0500

    Completando la clase Nodo con atributos y constructor

--- lista_reproduccion/src/Nodo.java
+++ lista_reproduccion/src/Nodo.java
@@ -1,3 +1,12 @@
 public class Nodo {
+        T key;
+        Nodo left, right;
+        int height;
     
+        public AVLNode(T key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+            this.height = 1;
+        }
 }
