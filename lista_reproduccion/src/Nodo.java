f98d3c8a0b205fa95822aa9c691589e319694c68
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-20 23:15:15 -0500

    Modificando nodo para lista enlazada

--- lista_reproduccion/src/Nodo.java
+++ lista_reproduccion/src/Nodo.java
@@ -1,12 +1,10 @@
 public class Nodo {
-        T key;
-        Nodo left, right;
-        int height;
-    
-        public AVLNode(T key) {
-            this.key = key;
-            this.left = null;
-            this.right = null;
-            this.height = 1;
-        }
+    T dato;
+    Nodo siguiente, anterior;
+
+    public Nodo(T dato){
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
 }
