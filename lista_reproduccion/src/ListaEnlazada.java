e65c74781b4c4785be6fffd5aa76b5159d83007b
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-20 23:22:44 -0500

    Creando metodo insertar para ListaEnlazada

--- lista_reproduccion/src/ListaEnlazada.java
+++ lista_reproduccion/src/ListaEnlazada.java
@@ -3,5 +3,17 @@ public class ListaEnlazada {
     public ListaEnlazada(){
         this.cabeza=null;
         this.cola = null;
+    }
+    public T insertar(T dato){
+        Nodo nodo = new Nodo(dato);
+        if(this.cabeza == null){
+            cabeza = cola = nodo;
+        }
+        else {
+            cola.siguiente = nodo;
+            nodo.anterior = cola;
+            cola = nodo;
+        }
+        return n.dato;
      }
 }
