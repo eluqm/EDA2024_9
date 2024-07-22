ea05c1311d140ed455cc2c0a885272c883c12bb0
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-22 11:40:09 -0500

    Añadiendo constructor y atributos para clase CargarDatos

--- lista_reproduccion/src/CargarDatos.java
+++ lista_reproduccion/src/CargarDatos.java
@@ -8,7 +8,19 @@ import com.opencsv.exceptions.CsvException;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.util.List;
-public class CargarDatos {
+public class CargarDatos extends SwingWorker<Void, Object[]>{
+    private final String archivo;
+    private final ListaEnlazada listaCanciones;
+    private final JTable tablaCanciones;
+    private final DefaultTableModel modeloTabla;
+    private int idgenerado;
     
+    public CargarDatosWorker(String archivo, ListaEnlazada listaCanciones, JTable tablaCanciones) {
+        this.archivo = archivo;
+        this.listaCanciones = listaCanciones;
+        this.tablaCanciones = tablaCanciones;
+        this.idgenerado=1;
+        this.modeloTabla = (DefaultTableModel) tablaCanciones.getModel();
+    }
     
 }
