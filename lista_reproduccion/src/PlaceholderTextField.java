38da96343d7115621b981569db9e8a47b4b51231
Veronica Cari Cruz <iorinagasehyouka@gmail.com>
2024-07-22 12:44:25 -0500

    Agregando constructor para que aparezca un texto en el textfield

---
+++ lista_reproduccion/src/PlaceholderTextField.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class PlaceholderTextField extends JTextField {
+    private String placeholder;
+
+    public PlaceholderTextField(String placeholder) {
+        this.placeholder = placeholder;
+        setText(placeholder);
+        setForeground(Color.GRAY);
+
+        addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (getText().equals(placeholder)) {
+                    setText("");
+                    setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (getText().isEmpty()) {
+                    setText(placeholder);
+                    setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+}
